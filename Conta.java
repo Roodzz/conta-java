78641959e26eab15f3d84fdb8fa727e7dce2bf82
Guilherme Rodrigues <guii-dev@hotmail.com>
2024-07-17 23:17:20 -0300

    alterações e testes realizados

--- Conta.java
+++ Conta.java
@@ -5,21 +5,31 @@ public class Conta {
     String nomeConta = "Guilherme Rodrigues";
     int numeroConta = 12345;
     double saldo = 1250.50;
-     public double deposito(double valorDeposito){
+     public double deposito(){
         System.out.print("Digite o valor a ser depositado: ");
-        valorDeposito = scanner.nextDouble();
+        double valorDeposito = scanner.nextDouble();
         return saldo = valorDeposito + saldo;
     };
-    public double saque(double valorSaque) {
+    public double saque() {
         System.out.print("Digite o valor a ser sacado: ");
-        valorSaque = scanner.nextDouble();
+        double valorSaque = scanner.nextDouble();
         if (saldo < valorSaque){
             System.out.print("Saldo insuficiente");
         }else {
-            return saldo = valorSaque - saldo;
-        }
-        System.out.print("Saldo: " + saldo);
+            saldo = saldo - valorSaque;
+        };
         return saldo;
-    }
+    };
+
+    public static void main (String [] args){
+
+        Conta conta = new Conta();
+        System.out.println("Saldo na conta: " + conta.saldo);
+        conta.deposito();
+        System.out.println("Saldo na conta: " + conta.saldo);
+        conta.saque();
+        System.out.println("Saldo na conta: " + conta.saldo);
+
+    };
 
 }
